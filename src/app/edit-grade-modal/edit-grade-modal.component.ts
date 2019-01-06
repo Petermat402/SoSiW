26d17a1b73463ea500d8b6bc1cedf98b66b43b3c
Peter <petermat402@gmail.com>
2019-01-06 12:35:41 +0100

    modals

---
+++ src/app/edit-grade-modal/edit-grade-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-grade-modal',
+  templateUrl: './edit-grade-modal.component.html',
+  styleUrls: ['./edit-grade-modal.component.scss']
+})
+export class EditGradeModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
